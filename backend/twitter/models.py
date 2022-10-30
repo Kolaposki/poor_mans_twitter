0c77af80d1947bb4a4b905c5b83436fb8f49f09c
Kolaposki <oshodikolapo@gmail.com>
2022-10-30 16:19:37 +0100

    Add tweet model

--- backend/twitter/models.py
+++ backend/twitter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    name = models.CharField(max_length=20)
+    message = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp", "name"]
+        verbose_name_plural = "Tweets"  # A human-readable name for the object, plural
+        verbose_name = "Tweet"  # A human-readable name for the object, singular
+
+    def __str__(self):
+        return f"{self.name} tweet"
